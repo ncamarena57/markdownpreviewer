b59b095dc5c568410b7c3620176128bdfe7a3948
Nathan <59515478+ncamarena57@users.noreply.github.com>
2024-08-09 11:19:48 -0700

    Display preview of text input

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const preview = document.getElementById("preview");
 const editor = document.getElementById("editor");
+
+editor.addEventListener('input', function() {
+    if (this.value.length > 1) {
+        preview.innerText = this.value;
+    }
+});
